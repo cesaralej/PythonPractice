a2ce02a5ce9b9a2a0ae31c4912e313f21791e5ce
unknown <cesar.ago2@gmail.com>
2019-01-21 10:37:02 -0500

    Moved Deck and Player code to modules

--- BlackJack/Deck.py
+++ BlackJack/Deck.py
@@ -1,25 +1,21 @@
-import random
-
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
-
 class Deck:
-	
-	def __init__(self):
-		createdeck()
-	
-	def createdeck(self):
-		self.cards = []
-		for rank in ranks:
-			for suit in suits:
-				cards = cards + [(suit, rank)];
 
-	def shuffle(self):
-		random.shuffle(self.cards);
+    def __init__(self):
+        self.cards = []
+        for rank in ranks:
+            for suit in suits:
+                self.cards = self.cards + [(rank, suit)]
 
-	def draw(self):
-		return cards.pop();
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-	def __str__(self):
-		print(cards)
+    def deal(self):
+        return self.cards.pop()
+
+    def __str__(self):
+        deckstring = "This is your deck:\n"
+
+        for card in self.cards:
+            deckstring = deckstring + f"{card[0]} of {card[1]} \n"
+
+        return deckstring
--- BlackJack/Player.py
+++ BlackJack/Player.py
@@ -1,49 +1,71 @@
 class Player:
 
-    def __init__ (self, balance=5000):
-        if name == "house":
-            self.balance = 5000
-            self.house = True
-        else:
-            self.balance = balance
+    def __init__(self, balance, house=False):
+        self.balance = balance
         self.hand = []
         self.value = 0
         self.aces = 0
-           
-    def bet(self, amount):
-        if self.balance >= amount:
-            self.balance = self.balance - amount
-        else:
-            print ("Funds Unavailable")
+        self.house = house
 
-    def draw(self):
-        hand = hand + deck.draw()
-        if ace in hand:
-            self.aces =+ 1
+    def bet(self, minbet):
+        while True:
+            bet = input(f"\nYou curently have ${self.balance}. Please type the amount of money you want to bet. \n")
+            try:
+                if self.balance >= int(bet):
+                    if int(bet) >= minbet:
+                        self.balance = self.balance - int(bet)
+                        print("\nBet Accepted!")
+                        return int(bet)
+                        break
+                    else:
+                        print(f"The minimum bet is {minbet}, please enter a higher amount of money!")
+                else:
+                    print("Funds Unavailable, please enter a lower amount of money!")
+                    continue
+            except ValueError:
+                print("Only use numbers!")
+                continue
 
-    def turn(self):
-        move = input('Hit or stand? ')
-        if move.lowercase() == ('h' or 'hit'):
-            hand = hand + deck.draw()
+    def resethand(self):
+        self.hand = []
+        self.aces = 0
 
     def calulatevalue(self):
+        self.value = 0
+        for card in self.hand:
+            self.value = self.value + values[card[0]]
         if self.aces > 0:
-            for card in hand:
-                value = values[card]
+            while self.value > 21:
+                self.value -= 10
+                self.aces -= 1
 
     def bust(self):
-        return sum(hand) > 21
+        return self.value > 21
 
-
-    def printhand(self):
-        print (hand)
+    def draw(self, card):
+        self.hand.append(card)
+        if card[0] == "Ace":
+            self.aces += 1
 
     def win(self, amount):
         self.balance = self.balance + amount
+        print(f'!!!You won ${amount}!!! Your new balance is ${self.balance}.')
 
     def showhand(self):
-        if house:
-            print(hand[1:])
-        else:
-            print(hand)
 
+        if self.house == False:
+            print('\nYour current hand is: ')
+            if len(self.hand) > 0:
+                time.sleep(1)
+                for card in self.hand:
+                    print(f'--{card[0]} of {card[1]}')
+                    time.sleep(1)
+            else:
+                print("There are no cards on your hand")
+
+        else:
+            print("\nThe House's hand is: ")
+            time.sleep(1)
+            for card in self.hand:
+                print(f'--{card[0]} of {card[1]}')
+                time.sleep(1)
