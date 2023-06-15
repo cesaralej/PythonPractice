006352092aae7455a8828181acafd7a0006d6ebf
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2023-06-15 16:32:34 +0200

    Day1: print, input, and variables

---
+++ 100_Days_Challenge/day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator.")
+city = input("What's the name of the city you grew up with?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
